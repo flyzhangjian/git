259d42c7a24c6a7aada88dbcc6dfd1d71723cf1d
zhangjian <1836810995@qq.com>
2015-12-24 13:29:29 +0800

    bug1

--- konhjian.py
+++ konhjian.py
@@ -7,21 +7,21 @@ __author__=zhangjian
 
  
 import pymysql.cursors
- connection = pymysql.connect(host='localhost',
+connection = pymysql.connect(host='localhost',
                              user='user',
                              password='a1836810995',
                              db='db',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
- try:
-     with connection.cursor() as cursor:
+try:
+    with connection.cursor() as cursor:
          sql = "insert into kongjian (id,shuoshuo) values(%s,%s)"
          cursor.execute(sql, ('happy pinganye'))
  
-     connection.commit()
+    connection.commit()
 
-     with connection.cursor() as cursor:
-         sql = "select 'id','shuoshuo' from kongjian
+    with connection.cursor() as cursor:
+         sql = "select 'id','shuoshuo' from kongjian"
          result=fetchone()
          print(result)
 finally:
