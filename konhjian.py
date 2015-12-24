97d42d3277270b1d8385936689f82cba7ba19897
zhangjian <1836810995@qq.com>
2015-12-24 13:22:01 +0800

    kongjian

---
+++ konhjian.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-b _*_
+
+'mysql'
+
+__author__=zhangjian
+
+ 
+import pymysql.cursors
+ connection = pymysql.connect(host='localhost',
+                             user='user',
+                             password='a1836810995',
+                             db='db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+ try:
+     with connection.cursor() as cursor:
+         sql = "insert into kongjian (id,shuoshuo) values(%s,%s)"
+         cursor.execute(sql, ('happy pinganye'))
+ 
+     connection.commit()
+
+     with connection.cursor() as cursor:
+         sql = "select 'id','shuoshuo' from kongjian
+         result=fetchone()
+         print(result)
+finally:
+	connection.close()
